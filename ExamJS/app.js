1a98974f3c477c4243dcfaa42c5c3dbd5e81e78f
turbo <test>
2022-04-05 17:54:48 +0300

    w_05_04_22

--- ExamJS/app.js
+++ ExamJS/app.js
@@ -1,17 +1,46 @@
-$(document).ready(function() {
-    $('.ap1').click(function(){ 
-          /*     $.ajax({
-            type: 'GET',
-            url: 'http://localhost:5219/User/GetAll',
-            dataType: 'json',
-            success: function (data) {
-                alert(JSON.stringify(data));
-            }
-        })*/
+var table = $('#table');
+
+$(document).ready(function() 
+{
+    $('.ap1').click(function()
+    { 
         $.getJSON("http://localhost:5219/User/GetAll")
-        .done(function(data) {
+        .done(function(data)
+        {
             alert(JSON.stringify(data));
-          })
+            FillTable(data);
+            console.log(data);
+        })
+        .fail(function()
+        {
+            console.log( "error" );
+        })
+        .always(function()
+        {
+            console.log( "complete" );
         });
-;
     });
+});
+
+
+
+function FillTable(data)
+{
+  let json = data;
+	table.html('');
+
+	for (let i = 0; i < json.length; i++)
+	{
+		let tr = $('<tr>');
+    console.dir(json[i]);
+	
+    let v = Object.values(json[i]);
+		for (let j = 0; j < v.length; j++) 
+		{
+			$('<td>').html(`${[j].value}`)
+			.appendTo(tr);
+    }
+    
+    table.append(tr);
+  }
+}
